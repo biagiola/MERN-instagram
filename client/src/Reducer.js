c5be03d448a37218e13ed1e1d25158360d5a72d5
David <41311088+biagiola@users.noreply.github.com>
2021-01-13 10:29:25 -0300

    get user login data and set its actiontype

--- client/src/Reducer.js
+++ client/src/Reducer.js
@@ -1,19 +1,32 @@
 export const initialState = {
-  user: null,
+  userName: null,
+}
+
+// if there is and user's value in the storage
+const data = JSON.parse(localStorage.getItem('user'))
+if (data) {
+  console.log('reducer data user', data)
+  initialState.user = data
 }
 
 export const actionTypes = {
-  SET_USER: 'SET_USER'
+  SET_USER: 'SET_USER',
+  SET_USER_LOCALSTORAGE: 'SET_USER_LOCALSTORAGE'
 }
 
 const reducer = (state, action) => {
-  
+
   console.log(action)
 
   switch (action.type) {
     case actionTypes.SET_USER:
       return {
         ...state,
+        userName: action.user
+      }
+
+    case actionTypes.SET_USER_LOCALSTORAGE:
+      return {
         user: action.user
       }
 
