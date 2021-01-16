4ef865747a64b8fce9be0a0a8dd532298bc0758a
David <41311088+biagiola@users.noreply.github.com>
2021-01-16 13:18:49 -0300

    Login footer added

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import TextField from '@material-ui/core/TextField'
+import { TextField, IconButton } from '@material-ui/core'
 import './Login.css'
 import ImageSlides from './ImageSlides'
 import { auth } from '../../firebase'
 import { actionTypes } from '../../Reducer'
 import { useStateValue } from '../../StateProvider'
+import PlayStore from '../../assets/playStore.png'
+import AppStore from '../../assets/appStore.png'
 
 const Login = () => {
   const [email, setEmail] = useState('')
@@ -65,55 +67,92 @@ const Login = () => {
 
   return (
     <div className="login__page">
-      <div className="login__page__left">
-        <ImageSlides/>
+      <div className="login__page__top">
+        <div className="login__page__left">
+          <ImageSlides/>
+        </div>
+        <div className="login__page__right">
+          <div className="login__signIn__form">
+            <img
+              className="app__headerImage"
+              src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+              alt="logo"
+            />
+            <form action="" className="login__page__form">
+              <TextField 
+                id="outlined-basic" 
+                label="Email" 
+                variant="outlined"
+                className="login__form__email"
+                type="text"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
+              <TextField 
+                id="outlined-basic" 
+                label="Password" 
+                variant="outlined"
+                className="login__form__password"
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+              <button 
+                disabled={!email}
+                onClick={handleLogin}
+                className="login__botton">
+                  Log In
+              </button>
+            </form>
+          
+            <div className="login__divider">
+              <hr/>
+              <span className="or__divider">OR</span>
+              <hr/>
+            </div>
+            
+            <div className="signUp">
+              <div className="signUp__title">Create an account</div>
+              <button 
+                disabled={!email}
+                className="signUp__botton"
+                onClick={handleSignUp}
+              >Sign Up</button>
+            </div>
+
+          </div>
+
+          <div className="getTheApp">Get the app.</div>
+          <div className="google__apple__stores">
+            <img src={PlayStore} width="128px"/>
+            <img src={AppStore} width="128px"/>
+          </div>
+        </div>
       </div>
-      <div className="login__page__right">
-        <img
-          className="app__headerImage"
-          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-          alt="logo"
-        />
-        <form action="" className="login__page__form">
-          <TextField 
-            id="outlined-basic" 
-            label="Email" 
-            variant="outlined"
-            className="login__form__email"
-            type="text"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-          <TextField 
-            id="outlined-basic" 
-            label="Password" 
-            variant="outlined"
-            className="login__form__password"
-            type="password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
-          <button 
-            disabled={!email}
-            className="login__botton"
-            onClick={handleLogin}
-          >Log In</button>
-        </form>
-        
-        <div className="login__divider">
-          <hr/>
-          <span className="or__divider">OR</span>
-          <hr/>
-        </div>
-        
-        <div className="signUp">
-          <div className="signUp__title">Create an account</div>
-          <button 
-            disabled={!email}
-            className="signUp__botton"
-            onClick={handleSignUp}
-          >Sign Up</button>
-        </div>
+      <div className="login__footer">
+        <ul>
+          <ul>
+            <li>About</li>
+            <li>Blog</li>
+            <li>Jobs</li>
+            <li>Help</li>
+            <li>API</li>
+            <li>Privacy</li>
+            <li>Terms</li>
+            <li>Top Accounts</li>
+            <li>Hashtags</li>
+            <li>Locations</li>
+          </ul>
+          <ul>
+            <li>Beauty</li>
+            <li>Dance & Performance</li>
+            <li>Fitness</li>
+            <li>Food & Drink</li>
+            <li>Home & Garden</li>
+            <li>Music</li>
+            <li>Visual Arts</li>
+          </ul>
+        </ul>
       </div>
     </div>
   )
